434a779d38070d9a30e1a49d430201faa01dc608
TurTurL <arthur.bidel@ensae.fr>
2024-03-10 02:38:07 +0100

    Fin de la séance 2 youhou plus que A* et la déco

--- swap_puzzle/graph.py
+++ swap_puzzle/graph.py
@@ -5,7 +5,8 @@ from grid import Grid
 import sys
 sys.path.insert(1, "/Users/arthurbidel/Documents/ENSAE/Informatique/S2/ensae-prog24-1/swap_puzzle")
 
-#fonction supplémentaire:
+# fonction supplémentaire:
+
 
 def make_hashable(item):
     """
@@ -19,12 +20,28 @@ def make_hashable(item):
     Output:
     -------
     item if item is an int.
-    item as a tuple[int], tuple[tuple[int]], etc. It has the same length as item.
+    item as a tuple[int], tuple[tuple[int]], etc.
+    It has the same length as item.
     """
     if not isinstance(item, int):
         return tuple(make_hashable(x) for x in item)  # Conversion récursive
     else:
         return item
+
+
+# fonctions pour passer de liste à tuple et inversement
+def tuples(n, grid):
+    for i in range(n):
+        grid[i] = tuple(grid[i])
+    return (tuple(grid))
+
+
+def lists(n, grid):
+    grid = list(grid)
+    for i in range(n):
+        grid[i] = list(grid[i])
+    return (grid)
+
 
 class Graph():
     """a
@@ -47,7 +64,7 @@ class Graph():
         The list of all edges
     """
 
-    def __init__(self, n, m):
+    def __init__(self, n, m, initial_grid=[]):
         """
         Initializes the graph with a set of nodes, and no edges.
 
@@ -56,14 +73,18 @@ class Graph():
         nodes: list, optional
             A list of nodes. Default is empty.
         """
+        self.n = n
+        self.m = m
+        if not initial_grid:
+            initial_grid = [list(range(i*n+1, (i+1)*n+1)) for i in range(m)]
         nodes = Grid(n, m).all_states()
         self.nodes = nodes
         self.graph = dict([(i, []) for i in nodes])
         self.nb_nodes = len(nodes)
         self.nb_edges = 0
         self.edges = []
-        self.n = n
-        self.m = m
+        self.initial_grid = initial_grid
+        self.grid = Grid(self.n, self. m, lists(self.n, initial_grid))
 
     def __str__(self):
         """
@@ -107,73 +128,131 @@ class Graph():
             self.graph[node2] = []
             self.nb_nodes += 1
             self.nodes.append(node2)
-        if node2 not in  self.graph[node1]:
+        if node2 not in self.graph[node1]:
             self.graph[node1].append(node2)
             self.graph[node2].append(node1)
             self.nb_edges += 1
             self.edges.append((node1, node2))
 
     def add_neighbours(self):
-        ''' On va créer les edges de notre graph, qui n'est défini que par la taille des grilles. On parcours tous les états, soit tous les nœuds et on ajoute les voisins dans les edges, il y a plusieurs boucles for qui ne servent qu'a alterner entre tuple et liste afin d'avoir le bon format pour les bonnes méthodes. '''
-        for tuples in Grid(self.n, self.m).all_states():
+        ''' On va créer les edges de notre graph, qui n'est défini que
+        par la taille des grilles. On parcours tous les états, soit tous
+        les nœuds et on ajoute les voisins dans les edges,
+        il y a plusieurs boucles for qui ne servent qu'a alterner
+        entre tuple et liste afin d'avoir le bon format pour
+        les bonnes méthodes. '''
+        for tuples1 in Grid(self.n, self.m).all_states():
             # passage de la grille en liste pour la méthode all_neighbours
-            grille = list(tuples)
-            for i in range(self.n):
-                grille[i] = list(grille[i])
+            grille = lists(self.n, tuples1)
 
             grille1 = Grid(self.n, self.m, grille)
 
             node1 = grille1.state
             for vosin in grille1.all_neighbours():
-                node2 = vosin 
+                node2 = vosin
                 # passage en tuple pour add_edge
-                for i in range(self.n):
-                    node1[i] = tuple(node1[i])
+                node1 = tuples(self.n, node1)
 
                 self.add_edge(tuple(node1), tuple(node2))
-                # repassage en liste pour la boucle 
-                for i in range(self.n):
-                    node1[i] = list(node1[i])
+                # repassage en liste pour la boucle
+                node1 = lists(self.n, node1)
+
         return None
 
-
-    def add_node (self, node):
+    def add_node(self, node):
         if node not in self.graph:
             self.graph[node] = []
             self.nb_nodes += 1
             self.nodes.append(node)
 
-
-    def bfs(self, src, dst):
+    def bfs1(self, src, dst):
         queue = [src]  # Utilise une liste pour stocker les nœuds à explorer
-        visited = set([src])  # Utilise un ensemble pour garder une trace des nœuds déjà visités
+        visited = set([src])
+        # Utilise un ensemble pour garder une trace des nœuds déjà visités
         paths = [[src]]  # Liste pour stocker les chemins
 
-        while queue: #tant que cette liste n'est pas vide
+        while queue:  # tant que cette liste n'est pas vide
             current_size = len(queue)  # Nombre de nœuds à explorer à ce niveau
-            new_paths = []  # Pour stocker les nouveaux chemins générés à ce niveau
+            new_paths = []
+            # Pour stocker les nouveaux chemins générés à ce niveau
 
             while current_size > 0:
-                current_node = queue.pop(0)  # Récupère le premier nœud de la queue
-                current_path = paths.pop(0)  # Récupère le chemin correspondant
+                current_node = queue.pop(0)
+                # Récupère le premier nœud de la queue
+                current_path = paths.pop(0)
+                # Récupère le chemin correspondant
 
                 if current_node == dst:
-                    return current_path  # Retourne le chemin trouvé dès la première solution
+                    return current_path
+                # Retourne le chemin trouvé dès la première solution
 
                 for neighbor in self.graph[current_node]:
                     if neighbor not in visited:
                         visited.add(neighbor)  # Marque le voisin comme visité
-                        queue.append(neighbor)  # Ajoute le voisin à la queue pour exploration
-                        new_paths.append(current_path + [neighbor])  # Génère le nouveau chemin
+                        queue.append(neighbor)
+                        # Ajoute le voisin à la queue pour exploration
+                        new_paths.append(current_path + [neighbor])
+                        # Génère le nouveau chemin
 
                 current_size -= 1  # Décrémente le compteur du niveau actuel
 
-            paths.extend(new_paths)  # Ajoute les nouveaux chemins à la liste des chemins pour l'exploration suivante
+            paths.extend(new_paths)
+            # Ajoute les nouveaux chemins à la liste des
+            # chemins pour l'exploration suivante
 
         return []  # Retourne une liste vide si aucun chemin n'est trouvé
 
+    def add_neighbours_with_bfs(self):
+        '''Cette fonction a pour but de construire le graph
+        en en regardant que la partie nécessaire à la résolution via BFS.
+        Pour cela on part de la destination et on utilise un code
+        similaire à bfs1, lorsque le chemin est trouvé on construit notre
+        graph en ajoutant le minimun des points parcours par le bfs
+        '''
+        queue = [Grid(self.n, self.m).state]
+        visited = set([tuples(self.n, Grid(self.n, self.m).state)])
+        paths = [[tuples(self.n, Grid(self.n, self.m).state)]]
 
-   """
+        while queue:
+            current_size = len(queue)
+            new_paths = []
+
+            while current_size > 0:
+                current_node = lists(self.n, queue.pop(0))
+                current_path = paths.pop(0)
+
+                if current_node == lists(self.n, self.initial_grid):
+                    current_size = 0
+                    queue = []
+                    path_found = current_path
+
+                else:
+                    for neighbor in Grid(self.n, self.m,
+                                         current_node).all_neighbours():
+                        if neighbor not in visited:
+                            visited.add(neighbor)
+                            queue.append(neighbor)
+                            new_paths.append(current_path + [neighbor])
+                    current_size -= 1
+            paths.extend(new_paths)
+
+        self.nodes = path_found
+        print(path_found)
+        self.graph = dict([(i, []) for i in tuples(len(path_found),
+                                                   path_found)])
+        self.nb_nodes = len(path_found)
+        self.nb_edges = 0
+        self.edges = []
+
+        for nodes in tuples(len(path_found), path_found):
+            if nodes != tuples(self.n, self.initial_grid):
+                for neighbours in Grid(self.n,
+                                       self.m, lists(self.n,
+                                                     nodes)).all_neighbours():
+                    self.add_edge(nodes, neighbours)
+        return None
+
+    """
     Question 7 :
     - Il faut d'abord construire le graphe de tous
     les états possible de la grille.
@@ -197,8 +276,6 @@ class Graph():
     #     -> déplace un nombre sur une ligne qui l'éloigne de sa ligne dst
     #     -> déplace un nombre sur une colonne qui l'éloigne de sa colonne dst
     #     """
-
-
 
     @classmethod
     def graph_from_file(cls, file_name):
@@ -258,7 +335,7 @@ class Graph():
     #     -> déplace un nombre sur une colonne qui l'éloigne de sa colonne dst
     #     """
 
-    def bfs(self, src, dst):
+    def bfs2(self, src, dst):
         i = 0
         queue = [src]
         liste = [src]
